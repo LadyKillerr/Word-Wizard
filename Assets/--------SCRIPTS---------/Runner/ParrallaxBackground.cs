4986bbc33533a5d29955fcf48e12c95f2ab07a7b
Lady Killer <dungvuduc0509@gmail.com>
2024-05-17 18:01:40 +0700

    feat: added runner game

---
+++ Assets/--------SCRIPTS---------/Runner/ParrallaxBackground.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ParrallaxBackground : MonoBehaviour
+{
+    Material backgroundMaterial;
+
+    [SerializeField] Vector2 moveSpeed;
+
+    Vector2 offset;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        backgroundMaterial = GetComponent<Image>().material;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MoveBackground();
+    }
+
+    void MoveBackground()
+    {
+        // gán offset + moveSpeed theo từng frame
+        offset = moveSpeed + offset;
+
+        // 
+        backgroundMaterial.mainTextureOffset = offset;
+    }
+
+}
